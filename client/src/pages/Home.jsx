a6041112232e50df9a4aeb5449ee82b433f4dcd8
Hector5280g <hectorgarcia342@yahoo.com>
2023-01-25 22:39:02 -0700

    Search bar frontend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,15 @@ import React, {useState, useEffect} from 'react'
 import {Loader, Card, FormField} from "../components"
 
 const RenderCards = ({data, title}) => {
-  
+  if (data?.length > 0) {
+    return data.map((post) => <Card key = {post._id}{...post}/>)
+  }
+return (
+  <h2 className='mt-5 font-bold text-[#FF0000] text-xl uppercase'>{title}</h2>
+)
+
 }
+
 const Home = () => {
 const [loading, setLoading] = useState(false)
 const [allPosts, setAllPosts] = useState(null)
@@ -34,7 +41,17 @@ const [searchText, setSearchText] = useState('')
     </h2>
   )}
   <div className='grid lg:grid-cols-4 sm:grid-cols-4 xs:grid-cols-2 grid-cols-1 gap-3'>
-
+{searchText ? (
+  <RenderCards
+  data={[]}
+  title='No search results found'
+  />
+): (
+  <RenderCards
+  data={[]}
+  title="No post found"
+  />
+)}
   </div>
   </>
 )}
