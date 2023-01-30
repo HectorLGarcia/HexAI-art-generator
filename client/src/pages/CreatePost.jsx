85c1b1b14928c403b1a6d16de370aae3acca40f1
Hector5280g <hectorgarcia342@yahoo.com>
2023-01-30 13:11:44 -0700

    commit

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://hexai.onrender.com', {
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
